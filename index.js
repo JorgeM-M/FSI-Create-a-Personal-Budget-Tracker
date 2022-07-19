ecc169eaff102cb07bbc7c7787974e91b5a503ac
JorgeM-M <jemm685@gmail.com>
2022-07-18 20:08:10 -0700

    added questions for weekly, monthly, and annual expenses on index.js

--- index.js
+++ index.js
@@ -3,3 +3,21 @@ let monthlyExpenses = 0
 let annualExpenses = 0
 
 // Your Code Here
+
+// weeklyExpenses
+for(let i = 0; i < weeklyExpenseQuestions.length; i++){
+    let answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
+    weeklyExpenses = weeklyExpenses + answer
+}
+
+// monthlyExpenses
+for(let i = 0; i < monthlyExpenseQuestions.length; i++){
+    let answer = parseFloat(window.prompt(monthlyExpenseQuestions[i]))
+    monthlyExpenses = monthlyExpenses + answer
+}
+
+// annualExpenses
+for(let i = 0; i < annualExpenseQuestions.length; i++){
+    let answer = parseFloat(window.prompt(annualExpenseQuestions[i]))
+    annualExpenses = annualExpenses + answer
+}
